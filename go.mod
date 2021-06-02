c313a586ac6ced6deb37a4bb93bb9e5f41c90866
yoruba-codigy <dammysteve10@gmail.com>
2021-06-02 23:30:58 +0100

    Initial Commit, Very Much WIP :tada:

---
+++ go.mod
@@ -0,0 +1,5 @@
+module personal_assistant
+
+go 1.16
+
+require github.com/yoruba-codigy/goTelegram/v2 v2.0.1
---
+++ models.go
@@ -0,0 +1,32 @@
+package main
+
+type data struct {
+	User             user             `json:"user"`
+	ExtendedEntities extendedentities `json:"extended_entities"`
+	RetweetCount     int              `json:"retweet_count"`
+	FavoriteCount    int              `json:"favorite_count"`
+}
+
+type user struct {
+	Name     string `json:"name"`
+	Username string `json:"screen_name"`
+}
+
+type extendedentities struct {
+	Media []media `json:"media"`
+}
+
+type media struct {
+	Type      string    `json:"type"`
+	VideoInfo videoinfo `json:"video_info"`
+}
+
+type videoinfo struct {
+	Variants []variants `json:"variants"`
+}
+
+type variants struct {
+	Bitrate     int    `json:"bitrate,omitempty"`
+	ContentType string `json:"content_type"`
+	URL         string `json:"url"`
+}
